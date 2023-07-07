fa71b71cb157988cd56e98919737a15d2a4579e2
Martin Ferreira <martin.f.yic@gmail.com>
2023-07-06 21:59:08 -0300

    feat: :sparkles: add Calendar at CalendarPage.jsx

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,54 @@
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { addHours } from 'date-fns';
 import { Navbar } from '../components';
+import { localizer, getMessagesES } from '../../helpers';
+
+const events = [
+	{
+		title: 'cumpleanos de martin',
+		notes: 'hay que organizar fiesta',
+		start: new Date(),
+		end: addHours(new Date(), 2),
+		bgColor: '#fafafa',
+		user: {
+			_id: '12332323',
+			name: 'Martin',
+		},
+	},
+];
 
 export const CalendarPage = () => {
+	const eventStyleGetter = (event, start, end, isSelected) => {
+		console.log({ event, start, end, isSelected });
+
+		const style = {
+			backgroundColor: '#347CF7',
+			borderRadius: '5px',
+			opacity: 0.8,
+			color: 'white',
+		};
+
+		return {
+			style,
+		};
+	};
+
 	return (
 		<>
 			<Navbar />
+
+			<Calendar
+				culture='es'
+				localizer={localizer}
+				events={events}
+				startAccessor='start'
+				endAccessor='end'
+				style={{ height: 'calc( 100vh - 80px )' }}
+				messages={getMessagesES()}
+				eventPropGetter={eventStyleGetter}
+			/>
 		</>
 	);
 };
