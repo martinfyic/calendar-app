655af1b91fca747b4ba805e68fa28ba0594c43f3
Martin Ferreira <martin.f.yic@gmail.com>
2023-07-09 20:08:26 -0300

    refactor: :zap: implement Metod: startSavingEvent & startDeletingEvent

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,20 +1,40 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { onSetActiveEvent } from '../store';
+import {
+	onSetActiveEvent,
+	onDeleteEvent,
+	onAddNewEvent,
+	onUpdateEvent,
+} from '../store';
 
 export const useCalendarStore = () => {
 	const dispatch = useDispatch();
 	const { events, activeEvents } = useSelector(state => state.calendar);
 
-	const SetActiveEvent = calendarEvent => {
+	const setActiveEvent = calendarEvent => {
 		dispatch(onSetActiveEvent(calendarEvent));
+	};
+
+	const startSavingEvent = async calendarEvent => {
+		if (calendarEvent._id) {
+			dispatch(onUpdateEvent({ ...calendarEvent }));
+		} else {
+			dispatch(onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }));
+		}
+	};
+
+	const startDeletingEvent = () => {
+		dispatch(onDeleteEvent());
 	};
 
 	return {
 		//* Propiedades
-		events,
 		activeEvents,
+		events,
+		hasEventSelected: !!activeEvents,
 
 		//* Metodos
-		SetActiveEvent,
+		startDeletingEvent,
+		setActiveEvent,
+		startSavingEvent,
 	};
 };
