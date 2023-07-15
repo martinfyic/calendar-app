a9b954ac468b4938fae4cf3ab9552ea6b14183e1
Martin Ferreira <martin.f.yic@gmail.com>
2023-07-15 19:29:47 -0300

    refactor: :recycle: implement authSlice in src/store/store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { uiSlice } from './ui';
-import { calendarSlice } from './calendar';
+import { authSlice, calendarSlice, uiSlice } from './';
 
 export const store = configureStore({
 	reducer: {
+		auth: authSlice.reducer,
 		calendar: calendarSlice.reducer,
 		ui: uiSlice.reducer,
 	},
