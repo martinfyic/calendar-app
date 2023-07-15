32178616ed0f67523c486e8e1a2a4aa043db4948
Martin Ferreira <martin.f.yic@gmail.com>
2023-07-15 19:28:35 -0300

    feat: :recycle: implement custom hook useForm in src/auth/page/LoginPage.jsx

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,17 +1,62 @@
+import { useForm } from '../../hooks';
 import './LoginPage.css';
 
+const loginFormFields = {
+	loginEmail: '',
+	loginPassword: '',
+};
+
+const registerFormFields = {
+	registerName: '',
+	registerEmail: '',
+	registerPassword: '',
+	registerPassword2: '',
+};
+
 export const LoginPage = () => {
+	const {
+		loginEmail,
+		loginPassword,
+		onInputChange: onLoginInput,
+	} = useForm(loginFormFields);
+
+	const {
+		registerName,
+		registerEmail,
+		registerPassword,
+		registerPassword2,
+		onInputChange: onRegisterInput,
+	} = useForm(registerFormFields);
+
+	const loginSubmit = event => {
+		event.preventDefault();
+		console.log({ loginEmail, loginPassword });
+	};
+
+	const registerSubmit = event => {
+		event.preventDefault();
+		console.log({
+			registerName,
+			registerEmail,
+			registerPassword,
+			registerPassword2,
+		});
+	};
+
 	return (
 		<div className='container login-container'>
 			<div className='row'>
 				<div className='col-md-6 login-form-1'>
 					<h3>Ingreso</h3>
-					<form>
+					<form onSubmit={loginSubmit}>
 						<div className='form-group mb-2'>
 							<input
 								type='text'
 								className='form-control'
 								placeholder='Correo'
+								name='loginEmail'
+								value={loginEmail}
+								onChange={onLoginInput}
 							/>
 						</div>
 						<div className='form-group mb-2'>
@@ -19,6 +64,9 @@ export const LoginPage = () => {
 								type='password'
 								className='form-control'
 								placeholder='Contraseña'
+								name='loginPassword'
+								value={loginPassword}
+								onChange={onLoginInput}
 							/>
 						</div>
 						<div className='d-flex justify-content-center align-items-center'>
@@ -33,12 +81,15 @@ export const LoginPage = () => {
 
 				<div className='col-md-6 login-form-2'>
 					<h3>Registro</h3>
-					<form>
+					<form onSubmit={registerSubmit}>
 						<div className='form-group mb-2'>
 							<input
 								type='text'
 								className='form-control'
 								placeholder='Nombre'
+								name='registerName'
+								value={registerName}
+								onChange={onRegisterInput}
 							/>
 						</div>
 						<div className='form-group mb-2'>
@@ -46,6 +97,9 @@ export const LoginPage = () => {
 								type='email'
 								className='form-control'
 								placeholder='Correo'
+								name='registerEmail'
+								value={registerEmail}
+								onChange={onRegisterInput}
 							/>
 						</div>
 						<div className='form-group mb-2'>
@@ -53,6 +107,9 @@ export const LoginPage = () => {
 								type='password'
 								className='form-control'
 								placeholder='Contraseña'
+								name='registerPassword'
+								value={registerPassword}
+								onChange={onRegisterInput}
 							/>
 						</div>
 
@@ -61,6 +118,9 @@ export const LoginPage = () => {
 								type='password'
 								className='form-control'
 								placeholder='Repita la contraseña'
+								name='registerPassword2'
+								value={registerPassword2}
+								onChange={onRegisterInput}
 							/>
 						</div>
 
